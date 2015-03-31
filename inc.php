1fb1970ec2ddea6e82d8b4bbe52926306d373d3e
paqure <dev@paqure.com>
2015-03-31 11:20:22 -0600

    Add Define Directive
    Define SQLAYER_ROOT for use by DCS datasource name and
    FilesystemIterator

--- inc.php
+++ inc.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * SQLayer LIBRARY INCLUDES
+ * @package   sqlayer
+ * @version   0.0.1
+ * @author    Roderic Linguri
+ * @copyright Copyright (c) 2015, Digices
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
 
-/*** SQLAYER LIBRARY INCLUDES ***/
+define('SQLAYER_ROOT',__DIR__);
 
-$fsi = new FilesystemIterator(__DIR__.'/lib');
+$fsi = new FilesystemIterator(SQLAYER_ROOT.'/lib');
 
 foreach ($fsi as $fil) {
 
-    require_once($fil);
-    
+    if(!$fil->isDot()) {
+
+        require_once($fil);
+
+    }
+
 }
